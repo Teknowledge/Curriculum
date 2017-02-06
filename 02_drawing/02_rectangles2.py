3a8357952a16f554a3e56fff04c7eaea69bd3aae
Erik Pintar <epintar@andrew.cmu.edu>
2017-02-06 11:05:49 -0500

    reorganized folders, added drawing lab code files

---
+++ 02_drawing/02_rectangles2.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+def draw(canvas, width, height):
+  # draw these on a piece of paper before you run the code
+  canvas.create_rectangle(0,0,50,50, fill="maroon")
+  canvas.create_rectangle(100,100,200,200, fill="pink")
+  canvas.create_rectangle(50,50,150,150, fill="turquoise")
+  # challenge rectangle! (add it to your drawing, then delete the #)
+  # canvas.create_rectangle(25,125,75,175, fill="purple")
+
+def runDrawing(width=200, height=200):
+  root = Tk()
+  canvas = Canvas(root, width=width, height=height)
+  canvas.pack()
+  draw(canvas, width, height)
+  root.mainloop()
+  print("bye!")
+
+runDrawing()
---
+++ 02_drawing/06_colorboard.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from random import randint
+
+def draw(canvas, width, height):
+  # write solution here, the first three rectangles are given to you
+  canvas.create_rectangle(0, 0, 20, 20, fill="darkblue")
+  canvas.create_rectangle(20, 0, 40, 20, fill="blue")
+  canvas.create_rectangle(40, 0, 60, 20, fill="lightblue")
+
+def runDrawing(width=200, height=200):
+  root = Tk()
+  canvas = Canvas(root, width=width, height=height)
+  canvas.pack()
+  draw(canvas, width, height)
+  root.mainloop()
+  print("bye!")
+
+runDrawing()
