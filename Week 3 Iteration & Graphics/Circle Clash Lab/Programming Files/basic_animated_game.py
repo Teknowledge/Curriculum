ff61cfbcb3f581a37c37886848e466df7ee6b5c2
Amal Nanavati <arnanava@andrew.cmu.edu>
2017-02-13 23:35:36 -0500

    added circle clash lab

---
+++ Week 3 Iteration & Graphics/Circle Clash Lab/Programming Files/basic_animated_game.py
@@ -0,0 +1,89 @@
+from tkinter import *
+import random
+
+# the game data for the initial game state
+def init():
+  data.playerX = 400
+  data.playerY = 300
+  data.winSpotX = 450
+  data.winSpotY = 450
+  data.gameOver = False
+
+# events updating the game data
+def keyPressed(event):
+  key = event.keysym
+  if key == "Right":
+    data.playerX += 5
+  elif key == "Left":
+    data.playerX -= 5
+  elif key == "Up":
+    data.playerY -= 5
+  elif key == "Down":
+    data.playerY += 5
+
+# the game data updating the game state
+def timerFired():
+  if playerAtWinSpot():
+    data.gameOver = True
+
+def playerAtWinSpot():
+  xDist = data.playerX - data.winSpotX
+  yDist = data.playerY - data.winSpotY
+  distApart = (xDist**2 + yDist**2)**0.5
+  closeDist = 50
+  return distApart < closeDist
+
+# the game state updating what is drawn
+def redrawAll(canvas):
+  canvas.create_oval(data.playerX - 10, data.playerY - 10, 
+                     data.playerX + 10, data.playerY + 10,
+                     fill="red")
+  if (data.gameOver):
+    winSpotText = "YOU WIN"
+  else:
+    winSpotText = "Move here"
+  canvas.create_text(data.winSpotX, data.winSpotY,
+                     text=winSpotText, font="Arial 20",
+                     anchor="center")
+
+
+
+# animation setup code below here #
+
+class Struct(object): pass
+data = Struct()
+
+def run(width=600, height=600):
+  def redrawAllWrapper(canvas):
+    canvas.delete(ALL)
+    redrawAll(canvas)
+    canvas.update()    
+
+  def keyPressedWrapper(event, canvas):
+    keyPressed(event)
+    redrawAllWrapper(canvas)
+
+  def timerFiredWrapper(canvas):
+    timerFired()
+    redrawAllWrapper(canvas)
+    # pause, then call timerFired again
+    canvas.after(data.timerDelay, timerFiredWrapper, canvas)
+
+  # Set up data and call init
+  data.width = width
+  data.height = height
+  data.timerDelay = 200 # milliseconds
+  init()
+  # create the root and the canvas
+  root = Tk()
+  canvas = Canvas(root, width=data.width, height=data.height)
+  canvas.pack()
+  # set up events
+  root.bind("<Key>", lambda event:
+                          keyPressedWrapper(event, canvas))
+  timerFiredWrapper(canvas)
+  # and launch the app
+  root.mainloop()  # blocks until window is closed
+  print("bye!")
+
+run()
