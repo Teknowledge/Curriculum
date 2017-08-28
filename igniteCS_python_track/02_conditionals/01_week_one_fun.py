2e2a5efd0961f4bb4ad7060ae41b7c4e4495c30b
Christopher George <cmgeorge@andrew.cmu.edu>
2017-08-28 07:59:21 -0400

    Add files via upload

---
+++ igniteCS_python_track/02_conditionals/01_week_one_fun.py
@@ -0,0 +1,72 @@
+# ^_^ WEEK 01 FUN ^_^
+
+input("Press enter to play a game.")
+
+print("In this game, I will ask you a tough question.")
+input("Press enter to accept the challenge.")
+
+print("What is 6 times 9?")
+print("Type your answer then press enter.")
+
+yourAnswer = input("> ")
+realAnswer = "42"
+
+
+if (yourAnswer == realAnswer):
+  print("Right! :-)")
+else:
+  print("Wrong, you lose.")
+
+
+# Challenge fun.1 - Play the game - what is wrong?
+# Change the code to be correct (can be the answer or the question or both).
+
+# Challenge fun.2 - Add two more questions to the game!
+
+
+#Place '#' at the beginning of the lines of code above to comment them out
+
+
+# Remove the '#' in the beginning of the lines of code below
+
+
+
+
+#input("Press enter to play a game.")
+
+#print("In this game, I will ask you a tough question.")
+#input("Press enter to accept the challenge.")
+
+#print("What is your favorite color?")
+#favColor = input("> ")
+
+
+
+#if favColor == 'red':
+#    print("Cool your favorite color is red!")
+#elif favColor != 'blue':
+#    print("Your favorite color isn't blue or red, it is " + favColor + ".")
+#else:
+#    print("Wow, your favorite color is blue!")
+
+
+
+#Challenge 1.1
+#What do you think this program will print if your favorite color is purple?
+
+
+#Challenge 1.2
+#What do you think this program will print if your favorite color is blue?
+
+#Challenge 1.3
+#Run the code and play the game!
+
+#Challenge 1.4
+#Change the code so if your favorite color is blue, the print statement under
+#else will print.
+
+# Challenge 1.5
+# play with the code to really understand the if, elif, and else along with the
+# ==, and !=
+
+
