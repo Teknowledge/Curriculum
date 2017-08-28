15f87daab6859e368dcaa69c41e9b94d4e96f1a2
Christopher George <cmgeorge@andrew.cmu.edu>
2017-08-28 07:55:40 -0400

    Add files via upload

---
+++ igniteCS_python_track/01_printing/01_02_syntax_practice.py
@@ -0,0 +1,31 @@
+# For each of the challenges below, write your code from scratch!
+#    Hint: The syntax guide will be helpful :)
+
+# Comment out or delete your code after finishing each challenge.
+
+# Challenge 2.1 - Write code to print out your name.
+
+
+
+# Challenge 2.2 - Write code to ask for your favorite color (but don't print
+#    anything).
+
+
+
+# Challenge 2.3 - Write code that first creates a variable called "faveColor"
+#    with the string value "blue" assigned to it.
+#    Then print out the value of the variable "faveColor".
+
+
+
+# Challenge 2.4 - Same as 2.3 but print out a message like:
+#    "Your favorite color is blue."
+#    using the variable "faveColor".
+
+
+
+# Challenge 2.5 - Same as 2.4 but combine it with 2.2 to ask for your favorite
+#    color and then print a message like:
+#    "Your favorite color is (inputted color)".
+
+
