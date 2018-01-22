73dd563aa79179726ec6079c4c4bc3d12a06cae9
Amal Nanavati <arnanava@andrew.cmu.edu>
2018-01-22 11:32:43 -0500

    Added hints to 1.1 activity

--- machine_learning_curriculum/1.1 Decision Stump/decisionStumpActivity.py
+++ machine_learning_curriculum/1.1 Decision Stump/decisionStumpActivity.py
@@ -15,7 +15,9 @@ import pickle
 # A note on terminology: a "class" or "label" is what we're trying to classify.
 # In our case, it can be either 0 or 1. In the flu example, cold = 0, flu = 1.
 #
-# Complete this activity by following the numbered challenges in order.
+# Complete this activity by following the numbered challenges in order. First,
+# try to do each challenge by yourself. If you need a hint, scroll to the bottom
+# of the file and there is pseudocode for each challenge.
 #
 #################################################################################################
 
@@ -84,11 +86,13 @@ class DecisionStump:
         # It then sets self.splitValue to the mean of the cells to the right and left of the
         # split. See Challenge 3 for the definition of a split.
 
-        # Initialize |minImpurity| to the maximum possible value of an impurity
-        minImpurity = 1
+        # Initialize |minImpurity| to greater than the maximum possible value of an impurity
+        minImpurity = 1.1
 
-        # We will update this value with (potentially) better splits.
-        bestSplitIndex = 0
+        # We will update this value with better splits. A "split" is an index in the
+        # list where we split the list into two parts: 1) everything to the left of
+        # the index, 2) everything to the right of the index, including the index.
+        bestSplitIndex = -1
 
         # Try all possible places to split the list.
         for possibleSplitIndex in range(len(self.trainingData)):
@@ -183,3 +187,88 @@ acc = testDecisionStump(studentDataPure, studentTest)
 # If your implementation is correct, this value should be 80%.
 # Why is the accuracy worse on the test data than on the training data?
 print ("Accuracy of our decision stump on the test data is", str(acc*100) + "%")
+
+################################################################################
+# BONUS CHALLENGE
+# Also test your decision stump implementation on the impure dataset, by
+# modifying the lines between CHALLENGE 7 and the BONUS CHALLENGE. If your
+# implementation is correct, your training accuracy should be 78.57142857142857%
+# and your test accuracy should be 66.66666666666666%. Why does it make sense
+# that our decision stump is less accurate for impure data?
+################################################################################
+
+
+################################################################################
+# HINTS
+################################################################################
+
+# CHALLENGE 2:
+# Use a for loop to iterate over every element in data. Each element in data is
+# a tuple, where the first element is the temperature and the the second element
+# is the label (0 for cold and 1 for flu). For example, data[i][1] == 0 if the
+# i-th datapoint is a cold, and data[i][1] == 1 if the i-th datapoint is a flu.
+# Before the for loop, create a variable to keep track of of the number of
+# zeroes in the list. Then within the for loop, if you see a zero, add one to
+# that variable. At the end of the for loop, determine the fraction of 0s in the
+# dataset and the fraction of 1s in the dataset by dividing numZeroes by the
+# length of the data, and by dividing (len(data) - numZeroes) by the len of the
+# data. Remember that you want to result to be a float, not an integer! Finally,
+# return 1 - (fraction 0s)**2.0 - (fraction 1s)**2.0
+
+# CHALLENGE 3:
+# Create two variables, leftList which stores all the elements from 0:i (note that
+# this excludes i) and rightList which includes all the elements from i:len(data).
+# (I am using i to refer to the index you split at). Then, calculate the impurity
+# of leftList and rightList using the function written in CHALLENGE 2. Next, you
+# have to calulate the weight for leftList and rightList. This is equal to the
+# length of the sublist, divided by the length of the overall data. Finally,
+# return leftWeight*leftImpurity + rightWeight*rightImpurity
+
+# CHALLENGE 4:
+# Call the getWeightedImpurity function to get the impurity of this split.
+# If it is less than minImpurity, set minImpurity to the new impurity, and set
+# bestSplit to the new split index. Since this is in a for loop, it will keep
+# going, continually updating the min impurity and best split until it actually
+# gets the best split out of all the elements.
+
+# CHALLENGE 5:
+# In this part, we are converting from the splitIndex to the splitValue. This is
+# necessary because the split index is tied to our specific training data. With
+# different data (like test data), it no longer makes sense to talk about an
+# index. Instead, we have to talk about the temperature value (i.e. if temp <
+# 98.6, predict cold, else predict flu). Therefore, for this challenge we need to
+# change the index to a temperature value. To do so, get the temperature of the
+# datapoint to the left of the index (i.e. self.trainingData[bestSplitIndex-1][0])
+# and the datapoint at that index, and average them to get the splitValue.
+
+# CHALLENGE 6:
+# Finally, in classify, you should check whether the datapoint (in this case, the
+# datapoint is just the temperature, since we don't know the label for test data)
+# is less than splitValue, in which case you will predict cold (0), or greater
+# than splitValue, in which case you will predict flu (1).
+
+# CHALLENGE 7:
+# Debugging is the hardest part, but it is also the most rewarding as you understand
+# why the code isn't working and finally get it to work. We can't tell you how to
+# fix problems in your code because everyone will have different bugs. However,
+# below are two general tips for debugging:
+#
+# (1) Take advantage of error messages, they are super useful! Python not only
+# gives you a brief description of the error (bottom line of the error message)
+# but also tells you what line it is on (third to bottom line of the error
+# message). These can very easily help you fix your code fast. Search online if
+# you don't know what an error message means.
+#
+# (2) In general print statements are very useful in debugging, since they can
+# show you what is happening behind-the-scenes while your code is being run.
+# We would recommend printing at various points in your code to understand
+# what is going wrong. For example, you could print the training data at the
+# beginning to make sure it is as you expect. You could print the impurity
+# every time you call the getImpurity function, to make sure it is reasonable.
+# You could print the bestSplitIndex in every iteration of the for loop, etc.
+# Basically, print where you think your code might be wrong, and if you are unsure,
+# print in multiple places to find out where your code is wrong.
+#
+# (3) If you re having trouble debugging, email us (cmu.teknowledge@gmail.com)
+# with your code, a brief description of what you have tried so far, and what
+# your ideas are, and we will send you suggestions.
--- machine_learning_curriculum/1.1 Decision Stump/decisionStumpActivitySolution.py
+++ machine_learning_curriculum/1.1 Decision Stump/decisionStumpActivitySolution.py
@@ -6,13 +6,13 @@ import pickle
 #
 # Various students have had their temperatures taken at the school infirmary.  As the infirmary
 # doctor you are trying to figure out if the students have a cold or a flu so you have a better
-# idea of what to do next to help them.  
+# idea of what to do next to help them.
 #
-# Based on previous student's temperatures and the illness that they had, you can use decision 
+# Based on previous student's temperatures and the illness that they had, you can use decision
 # stumps to find what a good dividing point is for a future student's temperature to predict whether
 # the student has a cold or flu!
 #
-#################################################################################################    
+#################################################################################################
 def getImpurity(data):
     if len(data) == 0:
         return 0
@@ -35,34 +35,34 @@ class DecisionStump:
     """
     This class represents a single Decision Stump. To create a decision stump,
     do "exampleStump = DecisionStump(data)". The data should be the elements of the training data
-    to train on. 
+    to train on.
     """
-    
+
     def __init__(self, trainingData):
         # |trainingData| is a list of tuples. A tuple in the list is the pair (value, class)
         # where |class| is either 0 or 1.
         # For example, a tuple could be (99.7, 0)
-        
+
         # Initialize class variables
         self.splitValue = 0
 
-        # First, we sort the data. Because the data is a list of tuples, we use a 
+        # First, we sort the data. Because the data is a list of tuples, we use a
         # special way to sort that takes a "lambda function." You don't have to worry
         # about how this works. Now the list is sorted by increasing values (temperatures).
         self.trainingData = sorted(trainingData, key=lambda x: x[0])
-    
+
     def train(self):
-        # This function finds the index that divides the two splits with the lowest impurity 
+        # This function finds the index that divides the two splits with the lowest impurity
         # It then sets self.splitValue to the mean of the cells to the right and left of the
         # split.
 
-        # Initialize |minImpurity| to the maximum possible value of an impurity
-        minImpurity = 1
+        # Initialize |minImpurity| to greater than the maximum possible value of an impurity
+        minImpurity = 1.1
 
-        # We will update this value with better splits. A "split" is an index in the 
+        # We will update this value with better splits. A "split" is an index in the
         # list where we split the list into two parts: 1) everything to the left of
         # the index, 2) everything to the right of the index, including the index.
-        bestSplitIndex = 0
+        bestSplitIndex = -1
 
         for possibleSplitIndex in range(len(self.trainingData)):
             thisImpurity = getWeightedImpurity(self.trainingData, possibleSplitIndex)
@@ -75,7 +75,7 @@ class DecisionStump:
         elif bestSplitIndex == len(self.trainingData) - 1:
             self.splitValue = float('inf')
         else:
-            self.splitValue = (self.trainingData[bestSplitIndex-1][0] 
+            self.splitValue = (self.trainingData[bestSplitIndex-1][0]
                     + self.trainingData[bestSplitIndex][0]) / 2
 
         print("Finished training the decision stump. Our split value is", self.splitValue)
@@ -113,7 +113,7 @@ def testDecisionStump(trainingData, testData):
         print("Test value:", testData[i][0], "Our prediction:", label, "Actual label:", testData[i][1])
         if label == testData[i][1]:
             numCorrect +=1
-    
+
     return float(numCorrect) / float(len(testData))
 
 
@@ -129,4 +129,14 @@ studentTest = loadDataFromFile("studentTest.p")
 error = testDecisionStump(studentDataPure, studentTest)
 print ("Accuracy of our decision stump on the test data is", str(error*100) + "%")
 
+studentDataImpure = loadDataFromFile("studentDataImpure.p")
+acc = testDecisionStump(studentDataImpure, studentDataImpure)
+# If your implementation is correct, this value should be 57.1428571429%.
+print ("Accuracy of our decision stump on the training data is", str(acc*100) + "%")
 
+
+studentTest = loadDataFromFile("studentTest.p")
+acc = testDecisionStump(studentDataImpure, studentTest)
+# If your implementation is correct, this value should be 66.6666666667%.
+# Why is the accuracy worse on the test data than on the training data?
+print ("Accuracy of our decision stump on the test data is", str(acc*100) + "%")
